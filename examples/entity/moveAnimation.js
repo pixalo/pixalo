299b5b0c6c44dcec155036c27193defb95d4036d
Salar <salarizdeveloper@gmail.com>
2025-09-21 04:19:29 +0330

    ​

--- examples/entity/moveAnimation.js
+++ examples/entity/moveAnimation.js
@@ -7,7 +7,7 @@
  *    - Salar Izadi
  *    - https://github.com/salarizadi
  */
-import Pixalo from '../../src/index.js';
+import Pixalo from 'http://cdn.jsdelivr.net/gh/pixalo/pixalo@master/dist/pixalo.esm.js';
 
 const game = new Pixalo('#canvas', {
     width : window.innerWidth,
